3a9f0ff64ce77951fff35b627b2e061b073194b1
Christian Winkler <christian.winkler@cwkr.de>
2025-06-27 10:45:10 +0200

    Fix UTF-8 encoding when displaying token content

--- internal/server/scripts/main.js
+++ internal/server/scripts/main.js
@@ -1,6 +1,17 @@
-export function decodeJwt(rawToken) {
+function decodeBase64(base64) {
+    const text = atob(base64);
+    const length = text.length;
+    const bytes = new Uint8Array(length);
+    for (let i = 0; i < length; i++) {
+        bytes[i] = text.charCodeAt(i);
+    }
+    const decoder = new TextDecoder(); // default is utf-8
+    return decoder.decode(bytes);
+}
+
+function decodeJwt(rawToken) {
     const base64 = rawToken.split('.')?.[1]?.replace('-', '+')?.replace('_', '/');
-    return JSON.parse(atob(base64));
+    return JSON.parse(decodeBase64(base64));
 }
 
 function rememberRequestForm() {
